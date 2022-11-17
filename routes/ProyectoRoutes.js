182509cd62709945b190d3548b09a6f2a4f5072c
alex <alejandro_mr7@outlook.com>
2022-11-16 18:33:18 -0600

    Creando el Routing y Funciones de Proyectos

---
+++ routes/ProyectoRoutes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const router = Router();
+const { obtenerProyectos, obtenerProyecto, nuevoProyecto, editarProyecto, eliminarProyecto, agregarColaborador, eliminarColaborador, obtenerTareas } = require('../controllers/ProyectoController');
+const checkAuth = require('../middlewares/checkAuth');
+
+router.route('/').get(checkAuth, obtenerProyectos).post(checkAuth, nuevoProyecto);
+router.route('/id').get(checkAuth, obtenerProyecto).put(checkAuth, editarProyecto).delete(checkAuth, eliminarProyecto);
+
+
+router.get('/tareas/:id', checkAuth, obtenerTareas);
+router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador);
+router.post('/eliminar-colaborador/:id', checkAuth, eliminarColaborador);
+
+
+module.exports = router;
